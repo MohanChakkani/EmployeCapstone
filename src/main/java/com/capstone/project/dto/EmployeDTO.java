2bd7584d30d35de694b3cdb18b7fdd0d83f41a2c
MohanChakkani <mohanchakkani263@gmail.com>
2023-04-27 20:00:25 +0530

    Build Changes

--- src/main/java/com/capstone/project/dto/EmployeDTO.java
+++ src/main/java/com/capstone/project/dto/EmployeDTO.java
@@ -2,10 +2,6 @@ package com.capstone.project.dto;
 
 import java.sql.Date;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Positive;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,13 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class EmployeDTO {
-	@Positive(message = "Employee ID must be a positive integer")
+	
     private int employeeID;
     
-    @NotBlank(message = "Employee name is required")
+   
     private String employeeName;
     
-    @NotNull(message = "Date of birth is required")
-    @Past(message = "Date of birth must be in the past")
+    
     private Date dateOfBirth;
 }
